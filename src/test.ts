e4079c72b781338de695c28fd198653d7de4a5e7
unknown <marcos.colon3@hotmail.com>
2024-09-02 12:17:37 -0300

    test passed

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
 import test from "ava";
-import { Product } from "./index";
+import { Product, User } from "./index";
 
 test("Testeo la clase Product", (t) => {
   // test Product class
@@ -7,4 +7,14 @@ test("Testeo la clase Product", (t) => {
   t.is(product.name, "Product 1");
   t.is(product.price, 10);
 });
-test("Testeo la clase User y sus métodos", (t) => {});
+test("Testeo la clase User y sus métodos", (t) => {
+  const user1 = new User("marcos caula");
+  const producto2 = new Product("product 2", 100); 
+  user1.addProduct(producto2);
+  console.log(user1.products);
+  user1.addProduct(new Product("producto 3", 23));
+  console.log(user1.products);
+  t.is(user1.name, "marcos caula");
+  t.is(producto2.name,"product 2");
+  t.is(producto2.price,100);
+});
